9e26479f7f2cf3aa42db7683ee0cb2476ff9a9a7
Ivy000000 <ivy9297@gmail.com>
2017-03-13 18:45:54 +0800

    LAB03_Part2
    重複量測 A0 腳位由電源供應器輸出之電壓
    程式可與步驟1 合併觀察

---
+++ Lab03_part2.ino
@@ -0,0 +1,18 @@
+float Analogin = 0;
+void setup() {
+   Serial.begin(9600);
+   pinMode(A0,INPUT);
+}
+
+void loop() {
+    int i;
+    float Vin;
+    for (i=0;i<=5;i++)
+      Analogin = 0.7* Analogin + 0.3* analogRead(A0);
+    
+    Vin = Analogin * 5 / 1024 ;
+    Serial.println(Vin);
+    delay(1000);
+    //Use Serial.print to Diaplay Vin;
+    //Sleep 0.5 sec
+ }
