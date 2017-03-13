fe6f7caaf35ae77d946c603b5ec1e5a274692d2e
Ivy000000 <ivy9297@gmail.com>
2017-03-13 18:11:46 +0800

    Lab03_part1
    觀察 Arduino 是否可透過 USB Serial 輸出
    根據 PC 之輸入 , 觀察 LED 的亮滅是否符合預期

---
+++ Lab03_part1.ino.ino
@@ -0,0 +1,35 @@
+const byte LED = 13;
+
+void setup() {
+
+     Serial.begin(9600);
+     Serial.println("Hello,World!");
+     
+}
+
+void loop(){
+     
+     int val = digitalRead(13);
+     if (Serial.available()) 
+     {
+       val = Serial.read();
+      
+       switch(val)
+       {
+          
+          case '0':
+          digitalWrite(LED,LOW);
+          Serial.println("LED OFF");
+          delay(1000);
+          break;
+        
+                
+          case '1':
+          digitalWrite(LED,HIGH);
+          Serial.println("LED ON");
+          delay(1000);
+          break;
+          
+       }
+     }
+}
